7ceb0e1ad46e4e07bf9ba4da9bf2b9caff191b92
hjyoo.dev <solteee23@gmail.com>
2022-01-07 20:45:01 +0900

    Update 2_video_download.py

--- 2_video_download.py
+++ 2_video_download.py
@@ -1,30 +1,26 @@
 import pytube
-
-
-for i in range(len(videolist)):
-# 영상 가져오기
-youtube = pytube.YouTube(videolist[i])
-videos = youtube.streams.filter(file_extension='mp4').all()
-#videos = youtube.streams.all()
-print(i, videolist[i])
-# 저장할 파일 경로
-parent_dir = "D:\KIRBS_PT_2\download"
-videos[0].download(parent_dir, titles[i])
-
-
-# mp4를 mp3로 변환하기
 import subprocess
 import os
+
+for i in range(len(videolist)):
+  youtube = pytube.YouTube(videolist[i]) # 영상 가져오기
+  videos = youtube.streams.filter(file_extension='mp4').all()
+  print(i, videolist[i])
+  parent_dir = "D:\download" # 저장할 파일 경로
+  videos[0].download(parent_dir, titles[i])
+
+
 default_filename = []
 new_filename = []
+
 for i in range(len(videolist)):
-default_filename.append(titles[i] + ".mp4")
-new_filename.append(titles[i] + ".mp3")
-origin_filename = str(default_filename[i])
-mp3_filename = str(new_filename[i])
-origin_path = os.path.join(parent_dir, origin_filename)
-mp3_path = os.path.join(parent_dir, mp3_filename)
-# mp4를 mp3로 변환
-subprocess.Popen(['ffmpeg', '-i', origin_path, mp3_path])
-print(i, mp3_filename)
+  default_filename.append(titles[i] + ".mp4")
+  new_filename.append(titles[i] + ".mp3")
+  origin_filename = str(default_filename[i])
+  mp3_filename = str(new_filename[i])
+  origin_path = os.path.join(parent_dir, origin_filename)
+  mp3_path = os.path.join(parent_dir, mp3_filename)  
+  subprocess.Popen(['ffmpeg', '-i', origin_path, mp3_path]) # mp4를 mp3로 변환
+  print(i, mp3_filename)
+ 
 print('done')
